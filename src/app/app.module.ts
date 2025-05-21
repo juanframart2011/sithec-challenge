ed4b13f2ca68d2ae8bd16eb2bc8e32d323af4ce4
juanframart2011 <juanframart2011@gmail.com>
2025-05-20 20:55:35 -0600

    agregando angular material y estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { PostListComponent } from './core/pages/blog/post-list/post-list.component';
+import { PostCardComponent } from './core/pages/blog/post-card/post-card.component';
+import { PostDetailComponent } from './core/pages/blog/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostListComponent,
+    PostCardComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
